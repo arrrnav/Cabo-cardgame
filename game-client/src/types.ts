ed06e8e2e1bc5e3f6da3e917782249be49d56878
arrrnav <arnav.chatty@gmail.com>
2026-01-02 02:20:11 -0800

    add backend type defining

--- game-client/src/types.ts
+++ game-client/src/types.ts
@@ -18,14 +18,16 @@ export interface GameState {
     current_turn: string | null;
 }
 
-// These interfaces define the "Contract" for our Socket events
+// Socket events
 export interface ServerToClientEvents {
+    //  defines functions that the server can call on the client
     room_update: (data: GameState) => void;
     deal_hand: (data: { hand: Card[] }) => void;
     error: (data: { message: string }) => void;
 }
 
 export interface ClientToServerEvents {
+    // defines functions that the client can call on the server
     join_game: (data: { room: string; name: string }) => void;
     start_game: (data: { room: string }) => void;
 }
